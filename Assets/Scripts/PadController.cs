9fb86eb5ee958e1e4e605376b06c062c6586a160
aaggarwal10 <anish.5@hotmail.com>
2021-03-27 17:39:54 -0400

    Billiard Table Done

--- Assets/Scripts/PadController.cs
+++ Assets/Scripts/PadController.cs
@@ -9,14 +9,14 @@ public class PadController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float h = Input.GetAxis("Horizontal")*2;
-        float v = Input.GetAxis("Vertical")*2;
+        float h = Input.GetAxis("Horizontal") * 2;
+        float v = Input.GetAxis("Vertical") * 2;
 
         rb.velocity = new Vector3(h, 0, v);
     }
